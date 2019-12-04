7acffa3e85907c22a7aa0cc9f5850d42205138e9
Alisha <alisha27011997@gmail.com>
2019-12-04 17:00:37 -0500

    TwoD Array

--- app/src/main/java/com/example/tictacdemo/MainActivity.java
+++ app/src/main/java/com/example/tictacdemo/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
  private int player = 0;
 
  private int[] gamesState = {-1,-1,-1,-1,-1,-1,-1,-1,-1};
+ private int[][] Array = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
     public void dropImage(View v){
         ImageView imageView = (ImageView) v;
         int state = Integer.valueOf(imageView.getTag().toString());
@@ -27,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         imageView.animate().translationYBy(1500).setDuration(300);
-        System.out.println("hello");
+
     }
 
                           @Override
